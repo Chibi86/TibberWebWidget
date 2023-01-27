a9c9d26cf380d6976389b3f24a6ba684ed4b24e9
Rasmus Berg <chibi86@hotmail.com>
2023-01-27 10:50:27 +0100

    fix: Bug fix for save settings part 2

--- widget.js
+++ widget.js
@@ -247,6 +247,7 @@ class Widget {
     }
 
     let avgPrice = 0;
+    this.prices = [];
 
     for (let i = this.startIndex; i <= this.endIndex; i++) {
       //   if (this.SOLOR_PRODUCER) {
@@ -412,19 +413,6 @@ class Widget {
   colorByPrice(price) {
     return price <= this.avgPrice ? this.TEXT_COLOR_LOW_PRICE : this.TEXT_COLOR_HIGH_PRICE;
   }
-
-  // lerpColor(a, b, amount) {
-
-  //     var ah = parseInt(a.replace(/#/g, ''), 16),
-  //         ar = ah >> 16, ag = ah >> 8 & 0xff, ab = ah & 0xff,
-  //         bh = parseInt(b.replace(/#/g, ''), 16),
-  //         br = bh >> 16, bg = bh >> 8 & 0xff, bb = bh & 0xff,
-  //         rr = ar + amount * (br - ar),
-  //         rg = ag + amount * (bg - ag),
-  //         rb = ab + amount * (bb - ab);
-
-  //     return '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1);
-  // }
 
   saveSettingsLocalStorage() {
     this.TIBBER_TOKEN = this.tibberTokenEl.value;
